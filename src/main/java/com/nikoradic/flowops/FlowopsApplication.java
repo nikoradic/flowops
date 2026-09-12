1c44b1770f0e1312b17ff3e28102372ce617ca5c
Nikola Radic <112083316+nikoradic@users.noreply.github.com>
2026-09-12 17:19:49 +0200

    Initial Spring Boot setup

---
+++ src/main/java/com/nikoradic/flowops/FlowopsApplication.java
@@ -0,0 +1,13 @@
+package com.nikoradic.flowops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowopsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowopsApplication.class, args);
+    }
+
+}
